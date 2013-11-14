5ca78c163d0dacafe650d25c062894d2bccf371e
Mikey <pmsorhaindo@gmail.com>
2013-11-14 09:35:36 +0000

    lift me rebrand

--- AndroidMapLocation/gen/uk/ac/sussex/addison/m/androidmaplocation/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package uk.ac.sussex.addison.m.androidmaplocation;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
