184a55dab2321b6a5679c4b1111b2fba643e77c7
eb357 <eb357@sussex.ac.uk>
2013-11-22 14:40:45 +0000

    Updated Main Activity

--- LiftMeClient/src/com/liftme/liftmeclient/MainActivity.java
+++ LiftMeClient/src/com/liftme/liftmeclient/MainActivity.java
@@ -77,18 +77,23 @@ public class MainActivity extends Activity implements LocationListener {
 	private Resources resourceVals;
 	private double lat = 0;
 	private double lng = 0;
-	
+
 	//user ID Window
-		Button  btnClosePopup;
-		PopupWindow UserIDWindow;
-		EditText UserID_Edit; 
-		String UserID;
+	Button  btnClosePopup;
+	PopupWindow UserIDWindow;
+	EditText UserID_Edit; 
+	String UserID;
 	//user ID Window
-	  
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		//Launching new Activity on selecting single List Item
+		Intent i = new Intent(getApplicationContext(), FirstPage.class);
+		//sending data to new activity
+		startActivity(i);
 
 		// show buttons
 		locationButton = (ImageButton) findViewById(R.id.locationBtn);
@@ -99,14 +104,14 @@ public class MainActivity extends Activity implements LocationListener {
 		if (initilizeMap()) {
 			onLocationChanged(location);			
 		} else {
-	        Toast.makeText(getBaseContext(),"Waiting for current location", Toast.LENGTH_LONG).show();
+			Toast.makeText(getBaseContext(),"Waiting for current location", Toast.LENGTH_LONG).show();
 		} // try and refresh maps
 		locationManager.requestLocationUpdates(provider, 20000, 10, this);
-		
+
 		//open pop-up window for user ID
 		this.putIDWindow();
 	} // method onCreate
-	
+
 	class saveBtnListener implements View.OnClickListener {
 		@Override
 		public void onClick(View v) {
@@ -115,43 +120,43 @@ public class MainActivity extends Activity implements LocationListener {
 				XMLExport xmlData = new XMLExport();
 				resourceVals = getResources();
 				String fileName = resourceVals.getString(R.string.xmlLocFileName) + currMarker.getMarkerDate() + currMarker.getMarkerTime() + ".xml";
-				
+
 				// attempt create on XML output
 				SDCard deviceSDCard = new SDCard();
 				if (deviceSDCard.writeToSD(xmlData.writeMarkerXml(currMarker), fileName, resourceVals)) {
-			        Toast.makeText(getBaseContext(),"Location saved: " + fileName, Toast.LENGTH_LONG).show();
+					Toast.makeText(getBaseContext(),"Location saved: " + fileName, Toast.LENGTH_LONG).show();
 				} else {
-			        Toast.makeText(getBaseContext(),"Error in writing to SD card", Toast.LENGTH_LONG).show();
+					Toast.makeText(getBaseContext(),"Error in writing to SD card", Toast.LENGTH_LONG).show();
 				} // end if can create xml output
-		    } else {
-		        Toast.makeText(getBaseContext(),"We haven't got a marker yet", Toast.LENGTH_LONG).show();
+			} else {
+				Toast.makeText(getBaseContext(),"We haven't got a marker yet", Toast.LENGTH_LONG).show();
 			} // end if got a marker
 		} // method onClick
-		
+
 	} // class buttonListener
 
 	class providerBtnListener implements View.OnClickListener {
 		@Override
 		public void onClick(View v) {
-// mtpa temp
+			// mtpa temp
 			// Launching new Activity on selecting single List Item
-//			Intent i = new Intent(getApplicationContext(), AndroidProviderList.class);
+			//			Intent i = new Intent(getApplicationContext(), AndroidProviderList.class);
 			// sending data to new activity
-//			startActivity(i);
-			
+			//			startActivity(i);
+
 			resourceVals = getResources();
 			XMLImport importFile = new XMLImport();
 			ConfirmRegister regData;
 			try {
 				String xmlData = importFile.readSDXMLData(resourceVals.getString(R.string.registerXmlDir), resourceVals.getString(R.string.registerXmlFileName));				
-		        XMLImport parserXML = new XMLImport();
-		        regData = parserXML.importConfirmRegister(xmlData);
-		        Toast.makeText(getBaseContext(),"Registered > " + regData.getDeviceID() + "<>" + regData.getSessionID(), Toast.LENGTH_LONG).show();
+				XMLImport parserXML = new XMLImport();
+				regData = parserXML.importConfirmRegister(xmlData);
+				Toast.makeText(getBaseContext(),"Registered > " + regData.getDeviceID() + "<>" + regData.getSessionID(), Toast.LENGTH_LONG).show();
 			} catch (Exception e) {
-		        Toast.makeText(getBaseContext(),"All fell apart reading in destinations", Toast.LENGTH_LONG).show();
+				Toast.makeText(getBaseContext(),"All fell apart reading in destinations", Toast.LENGTH_LONG).show();
 			}
 
-// mtpa temp
+			// mtpa temp
 		} // method onClick
 	} // class providerBtnListener
 
@@ -163,28 +168,28 @@ public class MainActivity extends Activity implements LocationListener {
 			// put the map on the screen
 			googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 
-	        // check if map is created successfully or not
+			// check if map is created successfully or not
 			if (googleMap == null) {
 				return false;
 			} else {
-		        // Enabling MyLocation Layer of Google Map
-		        googleMap.setMyLocationEnabled(true);
+				// Enabling MyLocation Layer of Google Map
+				googleMap.setMyLocationEnabled(true);
 
-		        // Getting LocationManager object from System Service LOCATION_SERVICE
-		        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		        if (locationManager == null) {
+				// Getting LocationManager object from System Service LOCATION_SERVICE
+				locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+				if (locationManager == null) {
 					return false;
 				} // if can't get a locationmanager
 
-		        // Getting the name of the best provider
-		        provider = locationManager.getBestProvider(new Criteria(), true);
-		        if (provider == "") {
+				// Getting the name of the best provider
+				provider = locationManager.getBestProvider(new Criteria(), true);
+				if (provider == "") {
 					return false;
 				}
 
-		        // Getting Current Location
-		        location = locationManager.getLastKnownLocation(provider);
-		        if (location == null) {
+				// Getting Current Location
+				location = locationManager.getLastKnownLocation(provider);
+				if (location == null) {
 					return false;
 				}
 				return true;
@@ -194,12 +199,12 @@ public class MainActivity extends Activity implements LocationListener {
 			return false;
 		}
 	} // method - initializeMap
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
 		if (initilizeMap()) {
-	        Toast.makeText(getBaseContext(),"Resume location : " + Double.toString(lat).substring(0, 5) + "," + Double.toString(lng).substring(0, 5), Toast.LENGTH_LONG).show();
+			Toast.makeText(getBaseContext(),"Resume location : " + Double.toString(lat).substring(0, 5) + "," + Double.toString(lng).substring(0, 5), Toast.LENGTH_LONG).show();
 		} else {
 			Toast.makeText(getApplicationContext(),"Waiting on exact location", Toast.LENGTH_SHORT).show();
 		} // try and refresh maps
@@ -214,7 +219,7 @@ public class MainActivity extends Activity implements LocationListener {
 
 		// set camera zoom level
 		float cameraZoom = (googleMap.getMinZoomLevel() + googleMap.getMaxZoomLevel())/2;
-		
+
 		// create and display marker
 		myMarker = new MarkerOptions();
 		myMarker.position(currPos);
@@ -233,9 +238,9 @@ public class MainActivity extends Activity implements LocationListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        Toast.makeText(getBaseContext(),"Current location : " + Double.toString(lat).substring(0, 5) + "," + Double.toString(lng).substring(0, 5), Toast.LENGTH_LONG).show();
+		Toast.makeText(getBaseContext(),"Current location : " + Double.toString(lat).substring(0, 5) + "," + Double.toString(lng).substring(0, 5), Toast.LENGTH_LONG).show();
 	} // onLocationChanged
-  
+
 	@Override
 	public void onProviderEnabled(String provider) {
 		Toast.makeText(this, "Enabled new provider " + provider,
@@ -252,59 +257,59 @@ public class MainActivity extends Activity implements LocationListener {
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	//pop-up ID window
-		public void putIDWindow()
-		{
-			 try { 
-		 	    	// get the instance of the LayoutInflater 
-		 	    	LayoutInflater inflater = (LayoutInflater) MainActivity.this 
-		 	    	.getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
-		 	    	View layout = inflater.inflate(R.layout.id_window,(ViewGroup)findViewById(R.id.popup_element)); 
-		 	    	
-		 	    	//create pop-up 
-		 	    	UserIDWindow = new PopupWindow(layout, 350, 350, true); 
-		 	    	UserIDWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
-		 	    	UserIDWindow.setFocusable(true);
-		 	    	
-		 	    	//class for ID EditBox
-		 	    	UserID_Edit   = (EditText)layout.findViewById(R.id.UserID);
-		 	    	
-		 	    	//Listener for Enter in User ID Text Box
-		 	    	UserID_Edit.setOnEditorActionListener(new OnEditorActionListener() {                     
-		 	    	    @Override
-		 	    	    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) { 	    	    	
-		 	    	    	try {  	 	    	    		
-			 				     UserID =  UserID_Edit.getText().toString();
-			 				     if (UserID.length() !=0)  UserIDWindow.dismiss();	 
-			 				     return true;		 				     
-			 				   }
-			 				 catch (Exception e) { 		 					 
-			 			 	    	e.printStackTrace(); 
-			 			 	    	return false;
-			 			 	      } 
-		 	    	    }
-		 	    	});
-		 	    	
-		 	    	btnClosePopup = (Button) layout.findViewById(R.id.btn_close_popup);
-		 	    	
-		 	    	//Listener for OK Button
-		 	    	btnClosePopup.setOnClickListener(new View.OnClickListener(){ 
-		 	    	@Override	
-		 	   		public void onClick(View v) { 	 				
-		 				try {  
-		 				     UserID =  UserID_Edit.getText().toString();
-		 				     if (UserID.length() !=0) UserIDWindow.dismiss();	
-		 				    }
-		 				 catch (Exception e) { 	 			 
-		 			 	    	    e.printStackTrace();} 		 
-		 				    }
-		 				});	 	    	
-			      }
-			 catch (Exception e) { 
-		 	    	e.printStackTrace(); 
-		 	      } 
-		 }	
+	public void putIDWindow()
+	{
+		try { 
+			// get the instance of the LayoutInflater 
+			LayoutInflater inflater = (LayoutInflater) MainActivity.this 
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE); 
+			View layout = inflater.inflate(R.layout.id_window,(ViewGroup)findViewById(R.id.popup_element)); 
+
+			//create pop-up 
+			UserIDWindow = new PopupWindow(layout, 350, 350, true); 
+			UserIDWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
+			UserIDWindow.setFocusable(true);
+
+			//class for ID EditBox
+			UserID_Edit   = (EditText)layout.findViewById(R.id.UserID);
+
+			//Listener for Enter in User ID Text Box
+			UserID_Edit.setOnEditorActionListener(new OnEditorActionListener() {                     
+				@Override
+				public boolean onEditorAction(TextView v, int actionId, KeyEvent event) { 	    	    	
+					try {  	 	    	    		
+						UserID =  UserID_Edit.getText().toString();
+						if (UserID.length() !=0)  UserIDWindow.dismiss();	 
+						return true;		 				     
+					}
+					catch (Exception e) { 		 					 
+						e.printStackTrace(); 
+						return false;
+					} 
+				}
+			});
+
+			btnClosePopup = (Button) layout.findViewById(R.id.btn_close_popup);
+
+			//Listener for OK Button
+			btnClosePopup.setOnClickListener(new View.OnClickListener(){ 
+				@Override	
+				public void onClick(View v) { 	 				
+					try {  
+						UserID =  UserID_Edit.getText().toString();
+						if (UserID.length() !=0) UserIDWindow.dismiss();	
+					}
+					catch (Exception e) { 	 			 
+						e.printStackTrace();} 		 
+				}
+			});	 	    	
+		}
+		catch (Exception e) { 
+			e.printStackTrace(); 
+		} 
+	}	
 } // class MainActivity
